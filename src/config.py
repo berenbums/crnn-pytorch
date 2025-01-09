70b454c7cce1cf31120817839151c4376a0bae14
Hendrik <55944332+berenbums@users.noreply.github.com>
2025-01-09 09:13:35 +0100

    Decrease learning rate, intervals

--- src/config.py
+++ src/config.py
@@ -12,17 +12,17 @@ train_config = {
     'epochs': 100,
     'train_batch_size': 8,
     'eval_batch_size': 512,
-    'lr': 0.0003,
+    'lr': 0.0001,
     'dropout': 0.5,
-    'show_interval': 2000,
-    'valid_interval': 4000,
-    'save_interval': 4000,
+    'show_interval': 3000,
+    'valid_interval': 6000,
+    'save_interval': 6000,
     'cpu_workers': 2,
     'reload_checkpoint': None,
     'valid_max_iter': 100,
     'decode_method': 'greedy',
     'beam_size': 10,
-    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch6/'
+    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch8/'
 }
 train_config.update(common_config)
 
