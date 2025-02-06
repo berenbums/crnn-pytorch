3f8770f1c2176a23cd69359064ba8f8ea3e62470
Hendrik <55944332+berenbums@users.noreply.github.com>
2025-02-06 12:18:09 +0200

    Update learning rate

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ train_config = {
     'epochs': 100,
     'train_batch_size': 32,
     'eval_batch_size': 512,
-    'lr': 0.0005,
+    'lr': 0.001,
     'weight_decay': 0.00001,
     'dropout': 0.5,
     'show_interval': 2000,
@@ -23,7 +23,7 @@ train_config = {
     'valid_max_iter': 100,
     'decode_method': 'greedy',
     'beam_size': 10,
-    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch15/'
+    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch16/'
 }
 train_config.update(common_config)
 
