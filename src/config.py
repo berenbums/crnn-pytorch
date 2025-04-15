210003d3f1de962bd41abe355251206ece7ffc0a
berenbums <55944332+berenbums@users.noreply.github.com>
2025-04-15 14:47:21 +0200

    Update checkpoint, char set

--- src/config.py
+++ src/config.py
@@ -23,14 +23,14 @@ train_config = {
     'valid_max_iter': 100,
     'decode_method': 'greedy',
     'beam_size': 10,
-    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch-css-2/'
+    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch-css-3/'
 }
 train_config.update(common_config)
 
 evaluate_config = {
     'eval_batch_size': 512,
     'cpu_workers': 2,
-    'reload_checkpoint': 'checkpoints/crnn_144000_loss0.707304867677832_acc0.9047303271441203.pt',
+    'reload_checkpoint': 'results/crnn-pytorch-css-2/crnn_148000_loss0.05423931501600545_acc0.9831838565022422.pt',
     'decode_method': 'beam_search',
     'beam_size': 10,
 }
--- src/dataset.py
+++ src/dataset.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class CssDataset(Dataset):
-    CHARS = ' 0123456789abcdefghPNBRQKBSLTDx+-=()/!?#.:'
+    CHARS = ' 0123456789abcdefghPNBRQKSLTDx+-=()/!?#.:'
     CHAR2LABEL = {char: i + 1 for i, char in enumerate(CHARS)}
     LABEL2CHAR = {label: char for char, label in CHAR2LABEL.items()}
 
