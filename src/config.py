7450cde7a2e13d07093c93aec92bca82abfbc184
berenbums <55944332+berenbums@users.noreply.github.com>
2025-11-27 08:24:17 +0200

    Reset weight decay

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ train_config = {
     'train_batch_size': 32,
     'eval_batch_size': 512,
     'lr': 0.001,
-    'weight_decay': 0.0001,
+    'weight_decay': 0.00001,
     'dropout': 0.5,
     'show_interval': 2000,
     'valid_interval': 4000,
@@ -22,7 +22,7 @@ train_config = {
     'reload_checkpoint': None,
     'decode_method': 'greedy',
     'beam_size': 10,
-    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch-css-10/'
+    'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/crnn-pytorch-css-11/'
 }
 train_config.update(common_config)
 
