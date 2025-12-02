44d6c6d12408767abaaa5cc93f1a07d8b603ab0b
Hendrik <55944332+berenbums@users.noreply.github.com>
2025-12-02 07:53:56 +0200

    Set checkpoint

--- src/config.py
+++ src/config.py
@@ -19,7 +19,7 @@ train_config = {
     'valid_interval': 4000,
     'save_interval': 4000,
     'cpu_workers': 2,
-    'reload_checkpoint': None,
+    'reload_checkpoint': '/content/drive/MyDrive/Colab Notebooks/resources/crnn_168000_loss0.2385731111974004_acc0.9447962822868259.pt',
     'decode_method': 'greedy',
     'beam_size': 10,
     'checkpoints_dir': '/content/drive/MyDrive/Colab Notebooks/output/'
